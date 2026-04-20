443dff7ebe41ad8259d6604c753797a21564a690
tanickell <tim.nickell@nebula-nlu.com>
2026-04-19 19:13:16 -0600

    Part 2-2: Review of dot product.

--- main.jl
+++ main.jl
@@ -177,6 +177,7 @@ println(dot(X[1, :], weights))
 ### PHASE 2 ###
 
 # Part 2-1: Vectors and Math
+println("Part 2-1:\n")
 
 v = [1.0, 2.0, 3.0]
 w = [4.0, 5.0, 6.0]
@@ -184,3 +185,35 @@ w = [4.0, 5.0, 6.0]
 println("v: ", v)
 println("w: ", w)
 
+
+println("\nAddition:")
+println(v + w)
+
+println("\nScalar multiplication:")
+println(2 * v)
+
+println("\nElement-wise multiplication:")
+println(v .* w)
+
+println("\nLength of v: ", length(v))
+println("First element of v: ", v[1])
+
+
+# Part 2-2: The Dot Product (Core of ML)
+# Measures how much two vectors "align"
+
+println("\n\nPart 2-2:")
+println("\nDot product (manual):")
+
+v = [1.0, 2.0, 3.0]
+w = [4.0, 5.0, 6.0]
+
+dot_manual = (v[1] * w[1]) + (v[2] * w[2]) + (v[3] * w[3])
+println(dot_manual)
+
+println("\nDot product (built-in):")
+println(dot(v, w))
+
+println("\nYour data dot product:")
+println(dot(X[1, :], weights))
+println(y_pred[1])
--- mathpractice.jl
+++ mathpractice.jl
@@ -1,6 +1,7 @@
 ### PHASE 2 ###
 
 # Part 2-1: Vectors and Math
+println("Part 2-1:\n")
 
 v = [1.0, 2.0, 3.0]
 w = [4.0, 5.0, 6.0]
@@ -21,4 +22,19 @@ println(v .* w)
 println("\nLength of v: ", length(v))
 println("First element of v: ", v[1])
 
+
+# Part 2-2: The Dot Product (Core of ML)
+# Measures how much two vectors "align"
+
+println("\nPart 2-2:\n")
+println("\nDot product (manual):")
+
+v = [1.0, 2.0, 3.0]
+w = [4.0, 5.0, 6.0]
+
+dot_manual = (v[1] * w[1]) + (v[2] * w[2]) + (v[3] * w[3])
+println(dot_manual)
+
+println("\nDot product (built-in):")
+println(dot(v, w))
 
